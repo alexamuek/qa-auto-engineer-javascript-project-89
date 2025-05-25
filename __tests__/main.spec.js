40945b69cd9811b06add25aceebd5ff559600551
Alex <alexamuek@gmail.com>
2025-05-25 22:13:41 +0800

    add first test

---
+++ __tests__/main.spec.js
@@ -0,0 +1,18 @@
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { test, expect } from "vitest";
+
+import Widget from '@hexlet/chatbot-v2';
+import steps from '@hexlet/chatbot-v2/example-steps';
+import '@hexlet/chatbot-v2/styles';
+
+
+test('positive test - initialize', async () => {
+  render(Widget(steps))
+  screen.debug()
+  const button = screen.getByRole('button', { name: 'Открыть Чат' })
+  expect(true).toBeTruthy();
+  expect(button).toBeVisible();
+  expect(button).toBeInTheDocument(); 
+  expect(button).not.toHaveStyle({ display: 'none' });
+})
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+     pool: "vmThreads",
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    css: true,
+  },
 })
