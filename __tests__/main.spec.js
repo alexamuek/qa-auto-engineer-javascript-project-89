81b4e28e8ef89888bb2508ee8f5cd7f3973f46fd
Alex <alexamuek@gmail.com>
2025-06-02 14:08:00 +0800

    add positive tests

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen, waitFor, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { test, expect, beforeEach, afterEach, vi } from "vitest"
 import { debug } from 'vitest-preview';
@@ -7,59 +7,131 @@ import Widget from '@hexlet/chatbot-v2'
 import steps from '../__fixtures__/steps.js'
 import '@hexlet/chatbot-v2/styles'
 
-let startButton
+let chatButton
+const scrollIntoViewMock = vi.fn()
+let container
 
 beforeEach(async () => {
-  //window.HTMLElement.prototype.scrollIntoView = vi.fn() // Мокаем метод
+  window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock // mock
 
-  render(Widget(steps))
-  startButton = screen.getByRole('button', { name: 'Открыть Чат' })
+  container = render(Widget(steps))
+  chatButton = await screen.findByRole('button', { name: 'Открыть Чат' })
   expect(true).toBeTruthy()
-  expect(startButton).toBeVisible()
-  expect(startButton).toBeInTheDocument() 
-  expect(startButton).not.toHaveStyle({ display: 'none' })
+  expect(chatButton).toBeVisible()
+  expect(chatButton).toBeInTheDocument() 
+  expect(chatButton).not.toHaveStyle({ display: 'none' })
 })
 
 afterEach(() => {
-  document.body.innerHTML = '' // Грубая очистка всего body
+  document.body.innerHTML = '' // clear body force
 })
 
 test('positive test - initialize', async () => {
   const user = userEvent.setup()
-  await user.click(startButton)
+  await user.click(chatButton)
   const [welcomeObj, ] = steps.filter((item) => item.id == 'welcome')
-  expect(document.body).toHaveTextContent(welcomeObj.messages[0])
-  const nextButton = screen.getByRole('button', { name: welcomeObj.buttons[0].text })
-  expect(nextButton).toBeVisible()
-  expect(nextButton).toBeInTheDocument() 
-  expect(nextButton).not.toHaveStyle({ display: 'none' })
+  await waitFor(() => {
+    expect(document.body).toHaveTextContent(welcomeObj.messages[0])
+  })
+  const nextButton = await screen.findByRole('button', { name: welcomeObj.buttons[0].text })
+  await waitFor(() => {
+    expect(nextButton).toBeVisible()
+    expect(nextButton).toBeInTheDocument() 
+    expect(nextButton).not.toHaveStyle({ display: 'none' })
+  })
   //screen.debug()  
 })
 
 test('positive test - close dialog', async () => {
   const user = userEvent.setup()
-  await user.click(startButton)
+  await user.click(chatButton)
   const [welcomeObj, ] = steps.filter((item) => item.id == 'welcome')
-  expect(document.body).toHaveTextContent(welcomeObj.messages[0])
-  const nextButton = screen.getByRole('button', { name: welcomeObj.buttons[0].text })
-  const closeButton = screen.getByRole('button', { name: 'Close' })
-  expect(closeButton).toBeVisible()
+  const nextButton = await screen.findByRole('button', { name: welcomeObj.buttons[0].text })
+  const closeButton = await screen.findByRole('button', { name: 'Close' })
+  await waitFor(() => {
+    expect(closeButton).toBeVisible()
+  })
   await user.click(closeButton)
-  const buttons = screen.queryAllByText('welcomeObj.buttons[0].text')
-  expect(buttons).toHaveLength(0)
+  const buttons = await screen.queryAllByText(welcomeObj.buttons[0].text)
+  await waitFor(() => {
+    expect(buttons).toHaveLength(0)
+  })
   // screen.debug() 
 })
 
 test('positive test - several steps', async () => {
   const user = userEvent.setup()
-  await user.click(startButton)
+  await user.click(chatButton)
   const [welcomeObj, ] = steps.filter((item) => item.id == 'welcome')
-  expect(document.body).toHaveTextContent(welcomeObj.messages[0])
-  const nextButton = screen.getByRole('button', { name: welcomeObj.buttons[0].text })
+  welcomeObj.messages.forEach(async (item) => {
+    await waitFor(() => {
+      // check: text appeared after opening of dialog
+      expect(document.body).toHaveTextContent(item)
+    })
+  })
+  const nextButton = await screen.findByRole('button', { name: welcomeObj.buttons[0].text })
+  await user.click(nextButton)
+  const paragraphs = await screen.findAllByText(welcomeObj.buttons[0].text);
+  await waitFor(() => {
+    // check: button was replaced by text message after click 
+    expect(paragraphs).toHaveLength(1);
+    expect(paragraphs[0].tagName).toBe('P')
+  })
+  const [startObj, ] = steps.filter((item) => item.id == welcomeObj.buttons[0].nextStepId)
+  startObj.buttons.forEach(async (button) => {
+    // check: after clicking elements appeared with role Button
+    const buttonEl = await screen.findByRole('button', { name: button.text })
+    await waitFor(() => {
+      // check: after clicking buttons appeared
+      expect(document.body).toHaveTextContent(button.text)
+    })
+  })
+  const [advansedButtonDescr, ] = startObj.buttons.filter((item) => item.nextStepId == 'advanced')
+  const advancedButton = await screen.findByRole('button', { name: advansedButtonDescr.text })
+  const scrollCount = scrollIntoViewMock.mock.calls.length
+  await user.click(advancedButton)
+  const [advancedObj, ] = steps.filter((item) => item.id == 'advanced')
+  await waitFor(() => {
+    // check: scroll was used
+    expect(scrollIntoViewMock.mock.calls.length).toBe(scrollCount + 1);
+  })
+  advancedObj.messages.forEach(async (item) => {
+    await waitFor(() => {
+      expect(document.body).toHaveTextContent(item)
+    })
+  })
+  advancedObj.buttons.forEach(async (button) => {
+    const foundButton = await screen.findByRole('button', { name: button.text })
+    await waitFor(() => {
+      expect(foundButton).toBeVisible()
+      expect(foundButton).toBeInTheDocument() 
+      expect(foundButton).not.toHaveStyle({ display: 'none' })
+      })
+  })
+  //debug();
+  //screen.debug(undefined, { maxDepth: 10, maxLength: 10000 }) 
+})
+
+test('positive test - scroll', async () => {
+  const user = userEvent.setup()
+  await user.click(chatButton)
+  const [welcomeObj, ] = steps.filter((item) => item.id == 'welcome')
+  const nextButton = await screen.findByRole('button', { name: welcomeObj.buttons[0].text })
   await user.click(nextButton)
   const [startObj, ] = steps.filter((item) => item.id == welcomeObj.buttons[0].nextStepId)
-  const advancedButton = screen.getByRole('button', { name: startObj.buttons[2].text })
+  const [advansedButtonDescr, ] = startObj.buttons.filter((item) => item.nextStepId == 'advanced')
+  const advancedButton = await screen.findByRole('button', { name: advansedButtonDescr.text })
   await user.click(advancedButton)
+  // находим див диалогового окна
+  const div1 = await screen.findByText('Виртуальный помощник');
+  const div2 = div1.parentElement
+  const modalBody = div2.nextElementSibling
+  // expect(div3).toHaveClass('modal-body');
+  // make scroll
+  fireEvent.scroll(modalBody, { target: { scrollTop:  100} });
+  await waitFor(() => {
+    expect(modalBody.scrollTop).toBe(100)
+  })
   //debug();
   //screen.debug(undefined, { maxDepth: 10, maxLength: 10000 }) 
 })
