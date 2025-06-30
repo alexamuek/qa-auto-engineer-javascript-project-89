f241a3ecfd6917221c7370418ad18e5704012074
Alex <alexamuek@gmail.com>
2025-06-30 22:29:00 +0800

    comment React

--- __tests__/app.spec.jsx
+++ __tests__/app.spec.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import { render, screen, waitFor, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { test, expect, beforeEach, afterEach, vi } from 'vitest'
